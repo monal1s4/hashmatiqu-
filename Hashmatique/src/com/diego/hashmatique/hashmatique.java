f7ef44f3b3cc571a1e306ad9cb2ae439d78dbb09
Diego <dev.diegoduran@gmail.com>
2023-06-30 19:48:46 -0400

    first commit

---
+++ Hashmatique/src/com/diego/hashmatique/hashmatique.java
@@ -0,0 +1,158 @@
+package com.diego.hashmatique;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class hashmatique {
+	public static void main(String[] args) {
+		Map<String, String> trackList = new HashMap<>();
+
+		trackList.put("Chemical", "Letra de la canción Chemical: Oxytocin makin' it all okay\r\n"
+				+ "When I come back down, it doesn't feel the same\r\n"
+				+ "Now I'm sittin' 'round, waitin' for the world to end all day\r\n"
+				+ "'Cause I couldn't leave you if I tried\r\n" + "You break me, then I break my rules\r\n"
+				+ "Last time was the last time too\r\n" + "It's fucked up, I know, but I'm still\r\n"
+				+ "Outside of the party, smokin' in the car with you\r\n"
+				+ "Seven Nation Army, fightin' at the bar with you\r\n"
+				+ "Tell you that I'm sorry, tell me what I gotta do\r\n" + "'Cause I can't let go, it's chemical\r\n"
+				+ "No, I can't let go, it's chemical\r\n" + "Every time I'm ready to make a change\r\n"
+				+ "You turn around and fuck out all my brains (woo)\r\n"
+				+ "I ain't tryna fight fate, it's too late to save face\r\n"
+				+ "I can't get away, maybe there's no mistakes (maybe there's no mistakes)\r\n"
+				+ "You break me, then I break my rules\r\n" + "Last time was the last time too\r\n"
+				+ "It's fucked up, I know, but I'm still\r\n"
+				+ "Standing outside of the party, smokin' in the car with you\r\n"
+				+ "Seven Nation Army, fightin' at the bar with you\r\n"
+				+ "Tell you that I'm sorry, tell me what I gotta do\r\n"
+				+ "'Cause I can't let go, it's chemical (chemical)\r\n"
+				+ "No, I can't let go, it's chemical (chemical)\r\n" + "No, I can't let go, it's chemical\r\n"
+				+ "I can't let go, it's chemical\r\n" + "Tell you that I'm sorry, tell me what I gotta do\r\n"
+				+ "No, I can't let go, it's chemica");
+		trackList.put("Sunflower", "Letra de la canción Sunflower: Ayy, ayy, ayy, ayy (ooh)\r\n"
+				+ "Ooh, ooh, ooh, ooh (ooh)\r\n" + "Ayy, ayy\r\n" + "Ooh, ooh, ooh, ooh\r\n"
+				+ "Needless to say, I keep her in check\r\n" + "She was a bad-bad, nevertheless (yeah)\r\n"
+				+ "Callin' it quits now, baby, I'm a wreck (wreck)\r\n"
+				+ "Crash at my place, baby, you're a wreck (wreck)\r\n"
+				+ "Needless to say, I'm keeping her in check\r\n" + "She was all bad-bad, nevertheless\r\n"
+				+ "Callin' it quits now, baby, I'm a wreck\r\n" + "Crash at my place, baby, you're a wreck\r\n"
+				+ "Thinkin' in a bad way, losin' your grip\r\n" + "Screamin' at my face, baby, don't trip\r\n"
+				+ "Someone took a big L, don't know how that felt\r\n" + "Lookin' at you sideways, party on tilt\r\n"
+				+ "Ooh-ooh-ooh\r\n" + "Some things you just can't refuse\r\n" + "She wanna ride me like a cruise\r\n"
+				+ "And I'm not tryna lose\r\n" + "Then you're left in the dust\r\n" + "Unless I stuck by ya\r\n"
+				+ "You're the sunflower\r\n" + "I think your love would be too much\r\n"
+				+ "Or you'll be left in the dust\r\n" + "Unless I stuck by ya\r\n" + "You're the sunflower\r\n"
+				+ "You're the sunflower\r\n" + "Every time I'm leavin' on you (ooh)\r\n"
+				+ "You don't make it easy, no (no, no)\r\n" + "Wish I could be there for you\r\n"
+				+ "Give me a reason to, oh (oh)\r\n" + "Every time I'm walkin' out\r\n"
+				+ "I can hear you tellin' me to turn around\r\n" + "Fightin' for my trust and you won't back down\r\n"
+				+ "Even if we gotta risk it all right now, oh (now)\r\n"
+				+ "I know you're scared of the unknown ('known)\r\n" + "You don't wanna be alone (alone)\r\n"
+				+ "I know I always come and go (and go)\r\n" + "But it's out of my control\r\n"
+				+ "And you'll be left in the dust\r\n" + "Unless I stuck by ya\r\n" + "You're the sunflower\r\n"
+				+ "I think your love would be too much\r\n" + "Or you'll be left in the dust\r\n"
+				+ "Unless I stuck by ya\r\n" + "You're the sunflower\r\n" + "You're the sunflower (yeah)");
+		trackList.put("rockstar", "Letra de la canción rockstar: Hahahahaha\r\n" + "Tank God\r\n" + "Ayy, ayy\r\n"
+				+ "I've been fuckin' hoes and poppin' pillies\r\n" + "Man, I feel just like a rockstar (ayy, ayy)\r\n"
+				+ "All my brothers got that gas\r\n" + "And they always be smokin' like a Rasta\r\n"
+				+ "Fuckin' with me, call up on a Uzi\r\n" + "And show up, man, them the shottas\r\n"
+				+ "When my homies pull up on your block\r\n"
+				+ "They make that thing go grrra-ta-ta-ta (ta, pow, pow, pow, ayy, ayy)\r\n"
+				+ "Switch my whip, came back in black\r\n"
+				+ "I'm startin' sayin', \"Rest in peace to Bon Scott\" (Scott, ayy)\r\n"
+				+ "Close that door, we blowin' smoke\r\n" + "She ask me light a fire like I'm Morrison ('son, ayy)\r\n"
+				+ "Act a fool on stage\r\n" + "Prolly leave my fuckin' show in a cop car (car, ayy)\r\n"
+				+ "Shit was legendary\r\n" + "Threw a TV out the window of the Montage\r\n"
+				+ "Cocaine on the table, liquor pourin', don't give a damn\r\n"
+				+ "Dude, your girlfriend is a groupie, she just tryna get in\r\n"
+				+ "Sayin', \"I'm with the band\" (ayy, ayy)\r\n"
+				+ "Now she actin' outta pocket, tryna grab up on my pants\r\n"
+				+ "Hundred bitches in my trailer say they ain't got a man\r\n"
+				+ "And they all brought a friend (yeah, ayy, ayy, ayy)\r\n"
+				+ "I've been fuckin' hoes and poppin' pillies\r\n" + "Man, I feel just like a rockstar (ayy, ayy)\r\n"
+				+ "All my brothers got that gas\r\n" + "And they always be smokin' like a Rasta\r\n"
+				+ "Fuckin' with me, call up on a Uzi\r\n" + "And show up, man, them the shottas\r\n"
+				+ "When my homies pull up on your block\r\n"
+				+ "They make that thing go grrra-ta-ta-ta (ta, pow, pow, pow)\r\n"
+				+ "I've been in the Hills fuckin' superstars\r\n" + "Feelin' like a popstar (21, 21, 21)\r\n"
+				+ "Drinkin' Henny, bad bitches jumpin' in the pool\r\n" + "And they ain't got on no bra (bra)\r\n"
+				+ "Hit her from the back, pullin' on her tracks\r\n"
+				+ "And now she screamin' out, \"¡No más!\" (yeah, yeah, yeah)\r\n"
+				+ "They like, \"Savage, why you got a twelve car garage\r\n" + "And you only got six cars?\" (21)\r\n"
+				+ "I ain't with the cakin', how you kiss that? (kiss that?)\r\n"
+				+ "Your wifey say I'm lookin' like a whole snack (big snack)\r\n"
+				+ "Green hundreds in my safe, I got old racks (old racks)\r\n"
+				+ "L.A. bitches always askin', \"Where the coke at?\" (21, 21)\r\n"
+				+ "Livin' like a rockstar, smash out on a cop car\r\n"
+				+ "Sweeter than a Pop-Tart, you know you are not hard\r\n"
+				+ "I done made the hot chart, 'member I used to trap hard\r\n"
+				+ "Livin' like a rockstar, I'm livin' like a rockstar (ayy)\r\n"
+				+ "I've been fuckin' hoes and poppin' pillies\r\n" + "Man, I feel just like a rockstar (ayy, ayy)\r\n"
+				+ "All my brothers got that gas\r\n"
+				+ "And they always be smokin' like a Rasta (yeah, yeah, yeah, yeah)\r\n"
+				+ "Fuckin' with me, call up on a Uzi\r\n" + "And show up, man, them the shottas\r\n"
+				+ "When my homies pull up on your block\r\n"
+				+ "They make that thing go grrra-ta-ta-ta (ta, grrra-ta-ta-ta-ta)\r\n"
+				+ "Star, star, rockstar, rockstar, star\r\n" + "Rockstar\r\n" + "Rockstar, feel just like a-\r\n"
+				+ "Rockstar\r\n" + "Rockstar\r\n" + "Rockstar\r\n" + "Feel just like a\r\n" + "");
+		trackList.put("Better now",
+				"Letra de la canción Better now: You prolly think that you are better now, better now\r\n"
+						+ "You only say that 'cause I'm not around, not around\r\n"
+						+ "You know I never meant to let you down, let you down\r\n" + "Woulda gave you anything\r\n"
+						+ "Woulda gave you everything\r\n" + "You know I say that I am better now, better now\r\n"
+						+ "I only say that 'cause you're not around, not around\r\n"
+						+ "You know I never meant to let you down, let you down\r\n" + "Woulda gave you anything\r\n"
+						+ "Woulda gave you everything, oh-oh\r\n"
+						+ "I did not believe that it would end, no (end, no)\r\n"
+						+ "Everything came second to the Benzo (Benzo)\r\n"
+						+ "You're not even speakin' to my friends, no\r\n"
+						+ "You knew all my uncles and my aunts though\r\n"
+						+ "20 candles, blow 'em out and open your eyes\r\n"
+						+ "We were lookin' forward to the rest of our lives\r\n"
+						+ "Used to keep my picture posted by your bedside\r\n"
+						+ "Now it's in your dresser with the socks you don't like\r\n"
+						+ "And I'm rollin', rollin', rollin', rollin'\r\n"
+						+ "With my brothers like it's Jonas, Jonas\r\n" + "Drinkin' Henny and I'm tryna forget\r\n"
+						+ "But I can't get this shit outta my head\r\n"
+						+ "You prolly think that you are better now, better now\r\n"
+						+ "You only say that 'cause I'm not around, not around\r\n"
+						+ "You know I never meant to let you down, let you down\r\n" + "Woulda gave you anything\r\n"
+						+ "Woulda gave you everything\r\n" + "You know I say that I am better now, better now\r\n"
+						+ "I only say that 'cause you're not around, not around\r\n"
+						+ "You know I never meant to let you down, let you down\r\n" + "Woulda gave you anything\r\n"
+						+ "Woulda gave you everything, oh-oh\r\n" + "I seen you with your other dude\r\n"
+						+ "He seemed like he was pretty cool\r\n" + "I was so broken over you\r\n"
+						+ "Life, it goes on, what can you do?\r\n"
+						+ "I just wonder what it's gonna take (what it's gonna take?)\r\n"
+						+ "Another foreign or a bigger chain (bigger chain)\r\n"
+						+ "Because no matter how my life has changed\r\n" + "I keep on looking back on better days\r\n"
+						+ "You prolly think that you are better now, better now\r\n"
+						+ "You only say that 'cause I'm not around, not around\r\n"
+						+ "You know I never meant to let you down, let you down\r\n" + "Woulda gave you anything\r\n"
+						+ "Woulda gave you everything\r\n" + "You know I say that I am better now, better now\r\n"
+						+ "I only say that 'cause you're not around, not around\r\n"
+						+ "You know I never meant to let you down, let you down\r\n" + "Woulda gave you anything\r\n"
+						+ "Woulda gave you everything, oh-oh\r\n" + "I promise, I swear to you, I'll be okay\r\n"
+						+ "You're only the love of my life (the love of my life)\r\n"
+						+ "You prolly think that you are better now, better now\r\n"
+						+ "You only say that 'cause I'm not around, not around\r\n"
+						+ "You know I never meant to let you down, let you down\r\n" + "Woulda gave you anything\r\n"
+						+ "Woulda gave you everything\r\n" + "You know I say that I am better now, better now\r\n"
+						+ "I only say that 'cause you're not around, not around\r\n"
+						+ "You know I never meant to let you down, let you down\r\n" + "Woulda gave you anything\r\n"
+						+ "Woulda gave you everything, oh-oh");
+
+		String tituloCancion = "Canción";
+
+		String letraCancion = trackList.get(tituloCancion);
+		System.out.println("Letra de la canción '" + tituloCancion + "': " + letraCancion);
+
+		System.out.println("Lista de canciones y letras:");
+		for (Map.Entry<String, String> entry : trackList.entrySet()) {
+			String titulo = entry.getKey();
+			String letra = entry.getValue();
+			System.out.println("Track: " + titulo);
+			System.out.println("Lyrics: " + letra);
+			System.out.println("---------------------------");
+		}
+	}
+}
